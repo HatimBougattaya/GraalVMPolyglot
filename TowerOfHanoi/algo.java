a6bb580041bf4d6ec71d597b2c5332d899973ad1
Hatim Bougattaya <hatim.bougattaya@gmail.com>
2021-04-29 16:05:56 +0000

    tree

---
+++ TowerOfHanoi/algo.java
@@ -0,0 +1,54 @@
+/********************************************************************************
+
+		THE ALGORITHM SOLVING TOWER OF HANOI PROBLEM
+
+*********************************************************************************/
+//import org.graalvm.polyglot.*;
+
+public class Algo{
+
+	// we can track movements
+	private int state;
+	//we dont really need a vector just the destination
+	private int travelVector;
+
+	//default
+	Algo(){
+		this(0,1);
+	}
+	//solution
+	public void tower(int n,int source,int median, int target){
+		if(n==1){
+			//last move
+			System.out.println(source+" "+target);
+		}else{
+			//iteration n-1 to median                               
+			tower(n-1,source,target,median);
+			
+
+			//n to target
+			this.moving();
+			//audit
+			this.travelVector = target ;
+														//in jsPoly:: System out == movePicture
+			System.out.println(source+" "+target);
+
+			
+			//progress ::::::::::::
+			//System.out.println("******progress------>"+n+"	"+source+"	"+median+"	"+target);
+			
+			//n-1 to target
+			tower(n-1,median,source,target);
+		}
+	}
+	public int moving(){
+		//maybe post incrementation is better in this polyglot situation
+		return state++;
+	}
+	public int progress(){
+		return state;
+	}
+	public int destination(){
+		return travelVector;
+	}
+}
---
+++ polyglot.js
@@ -0,0 +1,54 @@
+/********************************************************************************
+
+		POLYGLOT FOR THE SERVER RESPONSE 
+
+*********************************************************************************/
+
+
+//IMPORT
+const {initiateTower,drawTowers,resDrawing} = require("../utilFunc/libTower");
+const {initiateStack,drawStacks,loadDestination,move}= require("../utilFunc/libStack") ;
+
+const {drawHanoi,moveHanoi} = require("./utilFunc/drawImg");
+
+
+//REQUIRE
+const fs = require('fs');
+const { createCanvas, loadImage } = require('canvas');
+
+//INIT CANVACE
+const width = 1200;
+const height = 630;
+const canvas = createCanvas(width, height)
+const context = canvas.getContext('2d')
+
+//INIT JAVA AGENTS
+var algoBro = Java.type('Algo');
+var myHanoi = new algoBro();
+
+//auditMovements
+var changes = myHanoi.destination();
+var movements = myHanoi.progress();
+
+//INIT nJS AGENTS
+//Should be user input in NEXT VERSION 
+let stacksNum = 3;
+//draw and init stacks
+let actualStacks = drawHanoi(canvas,context,'./newStack.png',stacksNum);
+
+
+//SYNC WITH JAVA
+myHanoi.watch("changes",function(id, old, cur) {
+	 //see the actual work done
+	 console.log("Changed property: ", id);
+	 console.log("Original val: ", old);
+	 console.log("New val: ", cur);
+
+	 //CALL MOVE FUNC
+	 moveHanoi(canvas,context,'./newStack.png',actualStacks,old,cur);
+});
+
+//LETS GO
+myHanoi.tower(stacks,1,2,3);
+
+//console.log("Moveent done :" + movements);
